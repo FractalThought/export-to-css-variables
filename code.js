3d4ce42099154fa89557c3764a58c43f5d521d3d
Felix Thålin <felix.thalin@live.com>
2022-01-26 15:00:00 +0100

    complied code.js

--- code.js
+++ code.js
@@ -6,7 +6,7 @@ TODO: Save setting for selected color style and checked styles to export
 */
 figma.showUI(__html__, { width: 400, height: 400 });
 // Messages from the UI
-figma.ui.onmessage = (msg) => {
+figma.ui.onmessage = function (msg) {
     switch (msg.type) {
         case "generate":
             Generate(msg.options);
@@ -28,10 +28,10 @@ figma.ui.onmessage = (msg) => {
     }
 };
 function GetBaseStyle() {
-    const textStyles = figma.getLocalTextStyles().map((textStyle) => {
+    var textStyles = figma.getLocalTextStyles().map(function (textStyle) {
         return {
-            name: `${textStyle.name} (${textStyle.fontSize}px)`,
-            id: textStyle.id,
+            name: textStyle.name + " (" + textStyle.fontSize + "px)",
+            id: textStyle.id
         };
     });
     figma.ui.postMessage({ type: "populate-base-select", data: textStyles });
@@ -41,12 +41,12 @@ function SendMessage(message) {
 }
 // Main backend function, generate the styles and pass them to the UI
 function Generate(options) {
-    const data = {
+    var data = {
         colors: [],
         sizes: [],
         remBaseSize: {},
         remSizes: [],
-        effects: [],
+        effects: []
     };
     // No style checked
     if (!options.useColor && !options.useText && !options.useEffect) {
@@ -54,18 +54,18 @@ function Generate(options) {
         return;
     }
     if (options.useColor) {
-        const paintStyles = figma.getLocalPaintStyles().filter((paintStyle) => {
-            let color = paintStyle.paints[0];
+        var paintStyles = figma.getLocalPaintStyles().filter(function (paintStyle) {
+            var color = paintStyle.paints[0];
             return color.type === "SOLID";
         });
-        const colors = paintStyles.map((paintStyle) => {
-            let color = paintStyle.paints[0];
-            const rgb = {
+        var colors = paintStyles.map(function (paintStyle) {
+            var color = paintStyle.paints[0];
+            var rgb = {
                 red: BeautifyColor(color.color.r),
                 green: BeautifyColor(color.color.g),
-                blue: BeautifyColor(color.color.b),
+                blue: BeautifyColor(color.color.b)
             };
-            let ColorStyle = `rgba(${rgb.red}, ${rgb.green}, ${rgb.blue}, ${color.opacity})`;
+            var ColorStyle = "rgba(" + rgb.red + ", " + rgb.green + ", " + rgb.blue + ", " + color.opacity + ")";
             switch (options.colorStyle.toUpperCase()) {
                 case "HEX":
                     ColorStyle = RGBToHex(rgb);
@@ -76,49 +76,49 @@ function Generate(options) {
             }
             return {
                 name: FixNaming(paintStyle.name),
-                ColorStyle: ColorStyle,
+                ColorStyle: ColorStyle
             };
         });
         data.colors = colors;
     }
     if (options.useText) {
-        const textStyles = figma.getLocalTextStyles();
-        const textSizes = textStyles.map((textStyle) => {
+        var textStyles = figma.getLocalTextStyles();
+        var textSizes = textStyles.map(function (textStyle) {
             return {
                 name: FixNaming(textStyle.name),
                 size: textStyle.fontSize,
-                id: textStyle.id,
+                id: textStyle.id
             };
         });
         data.sizes = textSizes;
         if (options.useRem) {
-            const baseSize = textSizes.find((textStyle) => {
+            var baseSize_1 = textSizes.find(function (textStyle) {
                 return textStyle.id === options.selectedBase;
             });
-            if (baseSize === undefined) {
+            if (baseSize_1 === undefined) {
                 figma.notify("❗ Error: Selected base style doesn't exist.");
                 GetBaseStyle();
                 return;
             }
-            const remSizes = textSizes.map((textStyle) => {
-                const remSize = textStyle.size / baseSize.size;
+            var remSizes = textSizes.map(function (textStyle) {
+                var remSize = textStyle.size / baseSize_1.size;
                 return {
                     name: FixNaming(textStyle.name),
-                    size: +remSize.toFixed(2),
+                    size: +remSize.toFixed(2)
                 };
             });
-            data.remBaseSize = baseSize;
+            data.remBaseSize = baseSize_1;
             data.remSizes = remSizes;
         }
     }
     if (options.useEffect) {
-        const effectStyles = figma.getLocalEffectStyles();
-        const effects = effectStyles.map((effectStyle) => {
-            const { shadows, blur } = GenerateEffectStyle(effectStyle.effects);
+        var effectStyles = figma.getLocalEffectStyles();
+        var effects = effectStyles.map(function (effectStyle) {
+            var _a = GenerateEffectStyle(effectStyle.effects), shadows = _a.shadows, blur = _a.blur;
             return {
                 name: FixNaming(effectStyle.name),
                 shadows: shadows,
-                blur: blur,
+                blur: blur
             };
         });
         data.effects = effects;
@@ -132,27 +132,28 @@ function Generate(options) {
     figma.ui.postMessage({ type: "generated", data: data });
 }
 function GenerateEffectStyle(effects) {
-    const shadows = [];
-    let blur = ``;
-    effects.forEach((effect) => {
+    var shadows = [];
+    var blur = "";
+    effects.forEach(function (effect) {
         if (effect.type === "DROP_SHADOW" || effect.type === "INNER_SHADOW") {
             shadows.push(GenerateShadowStyle(effect)); // can have multiple shadows on a single element
         }
         else if (effect.type === "LAYER_BLUR") {
-            blur = `blur(${effect.radius}px)`; // can only have one blur-filter. Note: background_blur is not implemented in all browsers, so wont use that type of effect
+            blur = "blur(" + effect.radius + "px)"; // can only have one blur-filter. Note: background_blur is not implemented in all browsers, so wont use that type of effect
         }
     });
-    const result = {
-        shadows: `${shadows}`,
-        blur: blur,
+    var result = {
+        shadows: "" + shadows,
+        blur: blur
     };
     return result;
 }
-function GenerateShadowStyle({ type, color, offset, radius }) {
-    const alpha = Math.round(color.a * 100) / 100; // removes trailing numbers and beautifies the alpha (example: 0.05999943 becomes 0.06)
-    const rgba = `rgba(${BeautifyColor(color.r)}, ${BeautifyColor(color.g)}, ${BeautifyColor(color.b)}, ${alpha})`;
+function GenerateShadowStyle(_a) {
+    var type = _a.type, color = _a.color, offset = _a.offset, radius = _a.radius;
+    var alpha = Math.round(color.a * 100) / 100; // removes trailing numbers and beautifies the alpha (example: 0.05999943 becomes 0.06)
+    var rgba = "rgba(" + BeautifyColor(color.r) + ", " + BeautifyColor(color.g) + ", " + BeautifyColor(color.b) + ", " + alpha + ")";
     // If the effect is set as INNER_SHADOW, the shadow should be set to inset (this is how Figma shows it in the code-tab)
-    return `${type === "INNER_SHADOW" ? "inset" : ""} ${radius}px ${offset.x}px ${offset.y}px ${rgba}`;
+    return (type === "INNER_SHADOW" ? "inset" : "") + " " + offset.x + "px " + offset.y + "px " + radius + "px " + rgba;
 }
 // Reason for this to be a backend function is that the UI doesn't have access to the notify function
 function CopyToClipboard() {
@@ -163,14 +164,14 @@ function CopyToClipboard() {
 function FixNaming(name) {
     return CamelCaseToKebabCase(name
         .trim()
-        .replace("/", "--") // Figma uses / to separate different substyles, change this to BEM modifier
-        .replace(" ", "-") // Remove any spaces
+        .replace(/([\/])/g, "--") // Figma uses / to separate different substyles, change this to BEM modifier
+        .replace(/([\ ])/g, "-") // Remove any spaces
     ).toLowerCase();
 }
 function CamelCaseToKebabCase(name) {
-    return `${name.charAt(0)}${name
+    return "" + name.charAt(0) + name
         .substr(1)
-        .replace(/([a-z0-9]|(?=[A-Z]))([A-Z])/g, "$1-$2")}`; // camelCase to kebab-case
+        .replace(/([a-z0-9]|(?=[A-Z]))([A-Z])/g, "$1-$2"); // camelCase to kebab-case
 }
 // Figma stores the color value as a 0 to 1 decimal instead of 0 to 255.
 function BeautifyColor(colorValue) {
@@ -178,17 +179,17 @@ function BeautifyColor(colorValue) {
 }
 // Takes a single color (red, green, or blue) and changes it to hex
 function ColorToHex(rgb) {
-    let hex = Number(rgb).toString(16);
+    var hex = Number(rgb).toString(16);
     if (hex.length < 2) {
         hex = "0" + hex;
     }
     return hex.toUpperCase();
 }
 function RGBToHex(rgb) {
-    const red = ColorToHex(rgb.red);
-    const green = ColorToHex(rgb.green);
-    const blue = ColorToHex(rgb.blue);
-    return `#${red}${green}${blue}`;
+    var red = ColorToHex(rgb.red);
+    var green = ColorToHex(rgb.green);
+    var blue = ColorToHex(rgb.blue);
+    return "#" + red + green + blue;
 }
 function RGBToHSL(rgb, alpha) {
     // Make red, green, and blue fractions of 1
@@ -196,7 +197,7 @@ function RGBToHSL(rgb, alpha) {
     rgb.green /= 255;
     rgb.blue /= 255;
     // Find greatest and smallest channel values
-    let cmin = Math.min(rgb.red, rgb.green, rgb.blue), cmax = Math.max(rgb.red, rgb.green, rgb.blue), delta = cmax - cmin, hue = 0, saturation = 0, lightness = 0;
+    var cmin = Math.min(rgb.red, rgb.green, rgb.blue), cmax = Math.max(rgb.red, rgb.green, rgb.blue), delta = cmax - cmin, hue = 0, saturation = 0, lightness = 0;
     if (delta == 0)
         hue = 0;
     // Red is max
@@ -218,5 +219,5 @@ function RGBToHSL(rgb, alpha) {
     // Multiply l and s by 100
     saturation = +(saturation * 100).toFixed(1);
     lightness = +(lightness * 100).toFixed(1);
-    return `hsla(${hue}, ${saturation}%, ${lightness}%, ${alpha})`;
+    return "hsla(" + hue + ", " + saturation + "%, " + lightness + "%, " + alpha + ")";
 }
